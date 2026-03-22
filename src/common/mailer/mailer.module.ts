39ee2b2d2a86d6dee67f3053ccff78035db03955
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-03-22 14:15:04 +0200

    feat(admin): add AppMailerModule with Handlebars adapter

---
+++ src/common/mailer/mailer.module.ts
@@ -0,0 +1,40 @@
+import { Global, Module } from '@nestjs/common';
+import { MailerModule } from '@nestjs/mailer';
+import { HandlebarsAdapter } from '@nestjs/mailer/dist/adapters/handlebars.adapter';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { join } from 'path';
+import { AppMailerService } from './mailer.service';
+
+@Global()
+@Module({
+  imports: [
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
+    MailerModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        transport: {
+          host: config.get<string>('MAIL_HOST'),
+          port: config.get<number>('MAIL_PORT'),
+          secure: false,
+          auth: {
+            user: config.get<string>('MAIL_USER'),
+            pass: config.get<string>('MAIL_PASS'),
+          },
+        },
+        defaults: {
+          from: config.get<string>('MAIL_FROM'),
+        },
+        template: {
+          dir: join(__dirname, 'templates'),
+          // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call
+          adapter: new HandlebarsAdapter(),
+          options: { strict: true },
+        },
+      }),
+    }),
+  ],
+  providers: [AppMailerService],
+  exports: [AppMailerService],
+})
+export class AppMailerModule {}
