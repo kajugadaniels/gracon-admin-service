0a6ec8430926267a677309c2cdfd9740b99c2993
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-03-23 10:36:16 +0200

    chore(admin): export SkipAuth from decorators barrel

--- src/common/decorators/index.ts
+++ src/common/decorators/index.ts
@@ -1,3 +1,4 @@
 export { CurrentAdmin } from './current-admin.decorator';
 export type { AdminJwtPayload } from './current-admin.decorator';
 export { RequireRole, REQUIRE_ROLE_KEY } from './require-role.decorator';
+export { SkipAuth, IS_PUBLIC_KEY } from './skip-auth.decorator';
